5e8c4c417eed44d8cc15896b05a1419adf32713c
Soud <chnsydney@gmail.com>
2017-12-21 11:21:24 +0100

    install nav

---
+++ src/containers/Play.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Play extends Component {
+    render() {
+        return (
+            <Text>I'm the play container</Text>
+        )
+    }
+}
